d1e0eb201384197e1f5d131fb300c9884b7107b6
Anna Yu <ThoughtWorker@MacBook-Pro-3.local>
2017-01-23 23:32:32 -0600

    Optimizing import

--- src/test/java/com/java_email/SendMailIntegrationTest.java
+++ src/test/java/com/java_email/SendMailIntegrationTest.java
@@ -5,12 +5,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
 import org.subethamail.wiser.Wiser;
 
 import static com.java_email.WiserAssertions.assertReceivedMessage;
